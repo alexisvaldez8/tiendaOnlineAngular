a18178b7958bfc89bca123eeaf5c5dfe412dfd50
Edgar Alexis Mercado Valdez <15040440@itdurango.edu.mx>
2019-02-17 21:52:37 -0600

    logo y pruebas de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
+
+const routes:Routes = [
+  { path: '', component: HomeComponent 
+}]
+
 
 @NgModule({
   declarations: [
@@ -12,7 +18,8 @@ import { MenuComponent } from './menu/menu.component';
     MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
